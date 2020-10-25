fa62b0f450ea6acd11415f92e1a78cfbdd6fa493
Udayan Shevade <udayan.shevade@gmail.com>
2020-10-24 23:57:27 -0700

    minor: formatting

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,16 @@
-<main />
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main />
